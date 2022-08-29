4df0ca5e4868170b0d76659e31fa17aca4696686
Diego Matías Zurita <dmatiaszurita@gmail.com>
2022-08-29 12:13:35 -0300

    Adicion de NavBar con Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './components/NavBar';
+import NavBar from './components/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>
 );
